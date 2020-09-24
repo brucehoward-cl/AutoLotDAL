ab8fe0bd25346955ad7d1070622e7c1918a7e912
Bruce Howard <brucehoward@twc.com>
2020-09-24 13:50:22 -0400

    Through Chapter 22 Entity Framework

--- AutoLotDAL/EF/AutoLotEntities.cs
+++ AutoLotDAL/EF/AutoLotEntities.cs
@@ -1,19 +1,74 @@
 ﻿using System;
 using System.Data.Entity;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Infrastructure.Interception;
+using AutoLotDAL.Interception;
 using AutoLotDAL.Models;
 
 namespace AutoLotDAL.EF
 {
     public partial class AutoLotEntities : DbContext
     {
-        public AutoLotEntities() : base("name=AutoLotConnection") { }
+        //This is the built-in logging interceptor
+
+        public AutoLotEntities() : base("name=AutoLotConnection")
+        {
+            //Interceptor code
+            var context = (this as IObjectContextAdapter).ObjectContext;
+            context.ObjectMaterialized += OnObjectMaterialized;
+            context.SavingChanges += OnSavingChanges;
+        }
+
+        #region Interceptor loggers (simple and custom)
+        //static readonly DatabaseLogger DatabaseLogger = new DatabaseLogger("sqllog.txt", true); //true indicates whether the log should be appended to (default is false)
+        
+        //public AutoLotEntities() : base("name=AutoLotConnection")
+        //{
+        //    //DbInterception.Add(new ConsoleWriterInterceptor());  //This registers the Interceptor; registering them in code isolates them from changes to the configuration file
+        //    DatabaseLogger.StartLogging(); //This uses the built-in logging interceptor for simple logging
+        //    DbInterception.Add(DatabaseLogger); //This uses the built-in logging interceptor for simple logging
+        //} 
+
+        //This is necessary for the built-in interceptor logger
+        //protected override void Dispose(bool disposing)
+        //{
+        //    DbInterception.Remove(DatabaseLogger);
+        //    DatabaseLogger.StopLogging();
+        //    base.Dispose(disposing);
+        //}
+        #endregion
 
         public virtual DbSet<CreditRisk> CreditRisks { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
         public virtual DbSet<Inventory> Inventory { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
+
+        //fires just after SaveChanges() is called but before DB is updated
+        private void OnSavingChanges(object sender, EventArgs eventArgs)
+        {
+            //Sender is of type ObjectContext. Can get current and original values, and cancel/modify the save operation as desired.
+            var context = sender as ObjectContext;
+            if (context == null) return;
+
+            foreach (ObjectStateEntry item in context.ObjectStateManager.GetObjectStateEntries(EntityState.Modified | EntityState.Added))
+            {
+                //Do something important here
+                if ((item.Entity as Inventory) != null)
+                {
+                    var entity = (Inventory)item.Entity;
+                    if (entity.Color == "Red")
+                    {
+                        item.RejectPropertyChanges(nameof(entity.Color));
+                    }
+                }
+            }
+        }
+
+        //provides access to entities being reconstituted; fired just after model's props are populated but just before context serves it up
+        private void OnObjectMaterialized(object sender, System.Data.Entity.Core.Objects.ObjectMaterializedEventArgs e)
+        {
+        }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -22,5 +77,7 @@ namespace AutoLotDAL.EF
                 .WithRequired(e => e.Car)
                 .WillCascadeOnDelete(false);
         }
+
+
     }
 }
--- AutoLotTestDrive/Program.cs
+++ AutoLotTestDrive/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoLotDAL.EF;
-using AutoLotDAL.Models;
+﻿using AutoLotDAL.Models;
 using AutoLotDAL.Repos;
+using System;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
 
 namespace AutoLotTestDrive
 {
@@ -33,6 +29,9 @@ namespace AutoLotTestDrive
                     Console.WriteLine(c);
                 }
             }
+
+            TestConcurrency();
+            Console.ReadLine();
 
         }
         private static void AddNewRecord(Inventory car)
@@ -68,6 +67,37 @@ namespace AutoLotTestDrive
             using (var repo = new InventoryRepo())
             {
                 repo.Delete(carId, timeStamp);
+            }
+        }
+
+        //This method demonstrates a concurrency exception. Because Car object contains a timestamp, the second attempted update fails because
+        // that timestamp was updated in the first update. the exception was thrown and contains all of the info used in the attempt.
+        // dbValues are the connection values used, current and original values are as advertised.
+        private static void TestConcurrency()
+        {
+            var repo1 = new InventoryRepo();
+            //Use a second repo to make sure using a different context
+            var repo2 = new InventoryRepo();
+            var car1 = repo1.GetOne(1);
+            var car2 = repo2.GetOne(1);
+            car1.PetName = "NewName";
+            repo1.Save(car1);
+            car2.PetName = "OtherName";
+            try
+            {
+                repo2.Save(car2);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                var entry = ex.Entries.Single();
+                var currentValues = entry.CurrentValues;
+                var originalValues = entry.OriginalValues;
+                var dbValues = entry.GetDatabaseValues();
+                Console.WriteLine(" ******** Concurrency ************");
+                Console.WriteLine("Type\tPetName");
+                Console.WriteLine($"Current:\t{currentValues[nameof(Inventory.PetName)]}");
+                Console.WriteLine($"Orig:\t{originalValues[nameof(Inventory.PetName)]}");
+                Console.WriteLine($"db:\t{dbValues[nameof(Inventory.PetName)]}");
             }
         }
     }
